f1b12a2f19aa87b087e8964b130804bb7e3f1489
Gavin Kitch <kitchgavin@gmail.com>
2024-12-01 23:09:48 -0500

    Updating user module

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -3,17 +3,22 @@ class User {
   // final Color color;
   final int shotsMade;
   final int shotsTaken;
-  // final double shootingPercentage;
-  // final Map<String, double?> heatMap;
+  final double shootingPercentage;
+  final Map<int, double> heatMap;
 
   User({
-    required this.name,
+    required String name,
     // required this.color,
     required this.shotsMade,
     required this.shotsTaken,
-    // this.shootingPercentage,
-    // required this.heatMap,
-  });
+    required this.shootingPercentage,
+    required this.heatMap,
+  }) : name = '${name[0].toUpperCase()}${name.substring(1).toLowerCase()}';
+
+  double getShootingPercentage() {
+    double percentage = (shotsTaken > 0) ? (shotsMade / shotsTaken) * 100 : 0.0;
+    return double.parse(percentage.toStringAsFixed(2));
+  }
 
   factory User.fromJson(Map<String, dynamic> json) {
     return User(
@@ -21,10 +26,10 @@ class User {
       // color: json['color'],
       shotsMade: json['shotsMade'],
       shotsTaken: json['shotsTaken'],
-      // shootingPercentage: json['shootingPercentage'].toDouble(),
-      // heatMap: (json['heatMap'] as Map<String, dynamic>).map(
-      //   (key, value) => MapEntry(key, value?.toDouble()),
-      // ),
+      shootingPercentage: double.tryParse(json['shootingPercentage']) ?? 0.0,
+      heatMap: (json['heatMap'] as Map<dynamic, dynamic>).map(
+        (key, value) => MapEntry(int.parse(key), value?.toDouble() ?? 0.0),
+      ),
     );
   }
 }
