82c9722f2efbf04071e670ae34ed22c0639f5bfb
Gavin Kitch <kitchgavin@gmail.com>
2024-12-03 14:44:56 -0500

    Cleaning up heatmap

--- lib/widgets/heatmap.dart
+++ lib/widgets/heatmap.dart
@@ -28,7 +28,7 @@ class _HeatMapState extends State<HeatMap> {
   }
 
   void _loadImage() async {
-    // Load the asset image as a ui.Image
+    // load asset image as a ui.Image
     final ByteData data = await rootBundle.load(widget.imagePath);
     ui.decodeImageFromList(data.buffer.asUint8List(), (ui.Image img) {
       setState(() {
@@ -60,13 +60,6 @@ class _HeatMapPainter extends CustomPainter {
     required this.heatMap,
   });
 
-  // define color range
-  // List<HSVColor> get colorList => List<HSVColor>.generate(11, (i) {
-  //       double t = i / 10;
-  //       return HSVColor.lerp(startColor, endColor, t)!;
-  //     });
-
-  // List<Color> colorArr = [Colors.red, Colors.blue];
   List<Color> colorList = [
     Color(0xFF000080), // Dark Blue
     Color(0xFF0000FF), // Blue
@@ -105,7 +98,7 @@ class _HeatMapPainter extends CustomPainter {
     for (int i = 0; i < 5; i++) {
       // set color for segment based on percentage
       double zonePct = min(heatMap[i + 1] ?? 0.0, 99.9);
-      int zonePctIndex = (zonePct - 1) ~/ 10;
+      int zonePctIndex = (zonePct > 60.0) ? 9 : ((zonePct - 1) ~/ 6);
       Color zoneColor = colorList[zonePctIndex];
       overlayPaint.color = zoneColor.withOpacity(0.75);
 
@@ -115,8 +108,8 @@ class _HeatMapPainter extends CustomPainter {
           ..moveTo(centerX, centerY)
           ..arcTo(
             Rect.fromCircle(center: Offset(centerX, centerY), radius: radius),
-            -pi + i * segmentAngle, // Start angle
-            segmentAngle, // Sweep angle
+            -pi + i * segmentAngle,
+            segmentAngle,
             false,
           )
           ..close(),
@@ -127,116 +120,6 @@ class _HeatMapPainter extends CustomPainter {
 
   @override
   bool shouldRepaint(covariant CustomPainter oldDelegate) {
-    return true; // Repaint whenever properties change
+    return true;
   }
 }
-
-
-
-
-
-//   List<HSVColor> get colorList => List<HSVColor>.generate(steps, (i) {
-//         double t = i / steps;
-//         return HSVColor.lerp(startColor, endColor, t)!;
-//       });
-
-//   @override
-//   void paint(Canvas canvas, Size size) {
-//     // Draw the background image scaled to fit
-//     canvas.drawArc(Rect.largest, 0, pi, true, Paint());
-
-//     // // Paint heatmap points
-//     // Paint heatPaint = Paint()
-//     //   ..color = Colors.red.withOpacity(0.5)
-//     //   ..style = PaintingStyle.fill;
-
-//     // for (final point in heatmap) {
-//     //   canvas.drawCircle(point, 20, heatPaint); // Example circle for heatmap
-//     // }
-//   }
-
-//   @override
-//   bool shouldRepaint(covariant CustomPainter oldDelegate) {
-//     return true;
-//   }
-
-//   HSVColor getFillColor(percentage) {
-//     int ind = percentage ~/ 10;
-//     return colorList[ind];
-//   }
-// }
-
-
-// class _CalendarPainter extends CustomPainter {
-//   final DateTime dateTime;
-//   final Color iconColor;
-//   final Color textColor;
-
-//   _CalendarPainter({
-//     required this.dateTime,
-//     required this.iconColor,
-//     required this.textColor,
-//   });
-
-//   @override
-//   void paint(Canvas canvas, Size size) {
-//     IconData icon = FontAwesomeIcons.solidCalendar;
-//     // Paint the calendar icon
-//     TextPainter iconPainter = TextPainter(
-//         text: TextSpan(
-//           text: String.fromCharCode(icon.codePoint),
-//           style: TextStyle(
-//               fontSize:
-//                   size.width, // This will make the icon take the full width
-//               fontFamily: icon.fontFamily,
-//               package: icon.fontPackage,
-//               color: iconColor),
-//         ),
-//         textDirection: TextDirection.ltr);
-//     iconPainter.layout();
-//     iconPainter.paint(canvas, Offset(4, 0));
-
-//     // Paint the name of the day
-//     final dayName = DateFormat('EEEE', 'en_US').format(dateTime).toUpperCase();
-//     final TextPainter monthPainter = TextPainter(
-//       text: TextSpan(
-//         text: dayName,
-//         style: TextStyle(
-//           fontFamily: 'Oswald',
-//           fontWeight: FontWeight.w600,
-//           fontSize: size.width / 5.9, // Adjust as needed
-//           color: textColor,
-//         ),
-//       ),
-//       textDirection: TextDirection.ltr,
-//     );
-//     monthPainter.layout();
-//     monthPainter.paint(
-//       canvas,
-//       Offset(
-//         size.width / 2 - monthPainter.width / 2, // Centering horizontally
-//         size.height * .11, // Placing above the day number
-//       ),
-//     );
-
-//     // Paint the number of the month
-//     final dayNumber = DateFormat.d().format(dateTime);
-//     final TextPainter dayPainter = TextPainter(
-//         text: TextSpan(
-//           text: dayNumber,
-//           style: TextStyle(
-//               fontFamily: 'Oswald',
-//               fontWeight: FontWeight.w800,
-//               fontSize: size.width * .6, // Adjust as needed
-//               color: textColor),
-//         ),
-//         textDirection: TextDirection.ltr);
-//     dayPainter.layout();
-//     dayPainter.paint(
-//       canvas,
-//       Offset(
-//         size.width / 2 - dayPainter.width / 2, // Centering horizontally
-//         size.height * .2, // Centering vertically
-//       ),
-//     );
-//   }
